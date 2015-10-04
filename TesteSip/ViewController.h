195bc4289cec08ffdb77aeced054b9dae8149c18
fykos <bitbucket@elisnunes.com.br>
2015-10-04 14:49:02 -0300

    Initial Commit

---
+++ TesteSip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TesteSip
+//
+//  Created by Elis Nunes Ficos on 04/10/15.
+//  Copyright (c) 2015 TesteSip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
